fa07c647ccaf61c9f5d2d24cc0968586c61a17c9
Clay Dunston <dunstontc@gmail.com>
2018-05-29 12:10:48 -0500

    built my damn house on sand

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const json5 = require('gulp-json5-to-json');
 gulp.task('default', () => { });
 
 gulp.task('compile-json', () => {
-  gulp.src('./src/syntax/*.json5')
+  gulp.src('./src/v1/*.json5')
     .pipe(merge({
       fileName: "ruby.tmLanguage.json",
       json5: true,
